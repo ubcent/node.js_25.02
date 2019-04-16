2948a65e4bfd57b260172ad196340a67dc2f681b
bronskih <dima5202@mail.ru>
2019-04-16 21:44:25 +0500

    lesson-7

--- Dmitry_Bronskikh/lesson-5/app.js
+++ Dmitry_Bronskikh/lesson-5/app.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 const consolidate = require('consolidate');
 
 const app = express();
+const indexRouter = require('./routes');
+const taskRouter = require('./routes/task');
+const taskAPIRouter = require('./routes/API/task');
+const authAPIRouter = require('./routes/API/auth');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-const indexRouter = require('./routes/index');
-const taskRouter = require('./routes/task');
 
 app.engine('hbs', consolidate.handlebars);
 app.set('view engine', 'hbs');
@@ -15,5 +17,8 @@ app.set('views', 'lesson-5/views');
 
 app.use('/', indexRouter);
 app.use('/task', taskRouter);
+
+app.use('/api/auth', authAPIRouter);
+app.use('/api/task', taskAPIRouter);
 
 app.listen(8080);
---
+++ Dmitry_Bronskikh/lesson-5/routes/API/task.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const {Task} = require('../../models');
+const checkAuth = require('./checkAuth');
+
+router.use('/', checkAuth);
+router.use('/*', checkAuth);
+
+router.post('/', async (req, res) => {
+    const tasks = await Task.getAll();
+    res.json(tasks);
+});
+
+router.post('/:id', async (req, res) => {
+    const id = req.params.id;
+    const task = await Task.getOn(id);
+    res.json(task);
+});
+
+router.post('/create', async (req, res) => {
+    const newTask = req.body;
+    const id = await Task.add(newTask);
+    res.json(id);
+});
+
+router.post('/update/:id', async (req, res) => {
+    const id = req.params.id;
+    const updateTask = req.body;
+    updateTask.id = id;
+    await Task.update(updateTask);
+    res.json(id);
+});
+
+module.exports = router;
